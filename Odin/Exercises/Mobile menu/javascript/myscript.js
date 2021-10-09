8fd4afaafe179f9232226707a234d06f6b3a00ad
Niels Magendans <niels.magendans@gmail.com>
2021-10-09 15:00:21 +0200

    vooruitgang mobile project etc, 9 okt

---
+++ Odin/Exercises/Mobile menu/javascript/myscript.js
@@ -0,0 +1,4 @@
+// Opdracht is zogenaamde "scrollable navigatie", zelf uitgekozen.
+// Info die ik daarvoor ga gebruiken:
+// "Vloerkleden, banken, beddengoed, koken & tafelen, badtextiel, woonaccessoires, verlichting, kasten, gordijnen."
+
