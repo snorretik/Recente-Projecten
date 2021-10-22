dbcf29bfa6416e7a201fa9ea6b9b794effc9ebba
Niels Magendans <niels.magendans@gmail.com>
2021-10-22 17:59:09 +0200

    saving my attempts and progress on this assignment today

--- Odin/Exercises/Mobile menu/javascript/myscript.js
+++ Odin/Exercises/Mobile menu/javascript/myscript.js
@@ -2,3 +2,32 @@
 // Info die ik daarvoor ga gebruiken:
 // "Vloerkleden, banken, beddengoed, koken & tafelen, badtextiel, woonaccessoires, verlichting, kasten, gordijnen."
 
+function initializeTheStuff() {
+    let scrollableElement = document.querySelector('#bargonnascroll');
+    
+    let x = false;
+
+    window.addEventListener('mouseup', (e) => {
+        window.removeEventListener('mousemove', theEventItsself(e));
+    });
+
+    scrollableElement.addEventListener('mousedown', (e) => {
+        // let xAbsolutePos = []
+
+        x.addEventListener('bla', (e) => {})
+
+        scrollableElement.addEventListener('mousemove', theEventItsself(e));
+    })
+
+    function theEventItsself(variable) {
+
+        console.log(variable);
+        // if (xAbsolutePos.length == 0) {
+        //     xAbsolutePos.push();
+
+        // }
+    }
+
+}
+
+initializeTheStuff();
