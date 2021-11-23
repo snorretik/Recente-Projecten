c32f2243116808e147702adeee44786593aa192b
Niels Magendans <niels.magendans@gmail.com>
2021-11-23 14:11:13 +0100

    started new project for the Python learning

--- Odin/Exercises/Mobile menu/versie 2/javascript/myscript.js
+++ Odin/Exercises/Mobile menu/versie 2/javascript/myscript.js
@@ -33,32 +33,6 @@ function initializeTheStuff() {
             console.log("what the hell happened?")
         }
     });
-
-    // ------------------------------------------------
-
-    // window.addEventListener('mouseup', (e) => {
-    //     window.removeEventListener('mousemove', theEventItsself(e));
-    // });
-
-    // scrollableElement.addEventListener('mousedown', (e) => {
-    //     // let xAbsolutePos = []
-
-        
-
-    //     scrollableElement.addEventListener('mousemove', theEventItsself(e));
-    // })
-
-    // function theEventItsself(variable) {
-
-    //     console.log(variable);
-    //     // if (xAbsolutePos.length == 0) {
-    //     //     xAbsolutePos.push();
-
-    //     // }
-    // }
-
-    // ------------------------------------------------
-
 }
 
 initializeTheStuff();
