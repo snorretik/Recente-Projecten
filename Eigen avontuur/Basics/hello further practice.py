e068cbf1bb0d4564e13aac94b1d5311ab382f1ff
Niels Magendans <niels.magendans@gmail.com>
2021-11-23 21:33:11 +0100

    triad triad triad triad triad triad triad triad like tool

---
+++ Eigen avontuur/Basics/hello further practice.py
@@ -0,0 +1,15 @@
+# Dit gaat wat leuks doen (uiteraard)
+
+print('Hello, world!')
+print('What is your name?')    # Ask for their name
+
+myName = input()
+
+print('It is good to meet you, ' + myName)
+print('The length of your name is:')
+print(len(myName))
+
+print('What is your age?')     # Ask for their age
+myAge = input()
+
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
